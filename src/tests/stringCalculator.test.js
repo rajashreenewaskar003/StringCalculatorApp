69b013a1d596f8d476633c610b55f560ecf65c51
rajashreenewaskar003 <rajashree.newaskar003@gmail.com>
2025-08-06 00:02:41 +0530

    handle any amount of numbers

--- src/tests/stringCalculator.test.js
+++ src/tests/stringCalculator.test.js
@@ -9,4 +9,8 @@ describe('String Calculator', () => {
   it('returns 6 for "1,5"', () => {
     expect(add("1,5")).toBe(6);
   });
+
+   it('returns 10 for "1,2,3,4"', () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 });
